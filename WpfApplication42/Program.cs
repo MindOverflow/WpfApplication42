6115da0ce1d251c4718f4f6a25be8636642709c3
MindOverflow <toxaSmith@yahoo.com>
2017-03-20 21:55:38 +0300

    [add] Проверка на публичный/непубличный геттер и на наличие ссылки при помощи рефлексии.

--- WpfApplication42/Program.cs
+++ WpfApplication42/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace WpfApplication42
 {
@@ -6,11 +7,42 @@ namespace WpfApplication42
     {
         private static void Main()
         {
+            // Индикатор номера свойства. 
+            int counter = 0;
+
             // Цикл итерирует тип LogMessage по всем его свойствам, так как используется
             // Метод GetProperties() возвращающий массив PropertyInfo[].
             foreach (var publicPropInfo in typeof(LogMessage).GetProperties())
             {
-                Console.WriteLine(publicPropInfo.Name);
+                counter++;
+
+                // Возьми для свойства геттер (getter или ещё get-метод свойства).
+                var getMethod = publicPropInfo.GetGetMethod();
+
+                WriteLine($"{counter})  publicPropInfo.Name = {publicPropInfo.Name}... getMethod.Name = {getMethod.Name}");
+
+                // Если get-метод текущего свойства равен null.
+                if (getMethod == null)
+                {
+                    ForegroundColor = ConsoleColor.DarkRed;
+                    WriteLine($"{counter}) {getMethod.Name} равен null");
+                }
+                
+                // Если get-метод текущего свойства публичный.
+                if (getMethod.IsPublic)
+                {
+                    ForegroundColor = ConsoleColor.Green;
+                    WriteLine($"{counter}) {getMethod.Name} публичный геттер.");
+                }
+
+                // Если get-метод текущего свойства приватный.
+                if (getMethod.IsPrivate)
+                {
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine($"{counter}) {getMethod.Name} приватный геттер.");
+                }
+
+                ForegroundColor = ConsoleColor.Gray;
             }
         }
     }
