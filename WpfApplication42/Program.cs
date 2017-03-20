3e8edd272837880411c9592ca51166087fb8e5f6
MindOverflow <toxaSmith@yahoo.com>
2017-03-20 23:23:28 +0300

    [add] Добавил полную реализацию цикла foreach, как в классе LogMessageFormatString.

--- WpfApplication42/Program.cs
+++ WpfApplication42/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using static System.Console;
 
 namespace WpfApplication42
@@ -7,8 +8,18 @@ namespace WpfApplication42
     {
         private static void Main()
         {
-            // Индикатор номера свойства. 
-            int counter = 0;
+            // Словарь, в котором в качестве ключа хранится строковое представление 
+            // имени свойств класса LogMessage, все буквы в нижнем регистре.
+            // В качестве значения словаря хранится результат возвращаемого свойством
+            // объекта.
+
+            // Альтернативные способы задать один и тот же словарь. Считаю, что мой более правильный.
+            // var namedValues = new Dictionary<string, Func<LogMessage, object>>();
+            var namedValues = new Dictionary<string, object>();
+
+
+            // Индикатор номера свойства, реализованный ввиде свойства. 
+            var counter = 0;
 
             // Цикл итерирует тип LogMessage по всем его свойствам, так как используется
             // Метод GetProperties() возвращающий массив PropertyInfo[].
@@ -19,7 +30,15 @@ namespace WpfApplication42
                 // Возьми для свойства геттер (getter или ещё get-метод свойства).
                 var getMethod = publicPropInfo.GetGetMethod();
 
-                WriteLine($"{counter})  publicPropInfo.Name = {publicPropInfo.Name}... getMethod.Name = {getMethod.Name}");
+                // Если get-метод текущего свойства равен null или он не публичный:
+                if (getMethod == null || getMethod.IsPublic == false)
+                {
+                    continue; // Перейти к новой итерации, при выполнении данных условий.
+                }
+
+                WriteLine($"{counter})  publicPropInfo.Name.ToLower() = {publicPropInfo.Name.ToLower()}");
+                WriteLine(
+                    $"{counter})  publicPropInfo.Name = {publicPropInfo.Name}... getMethod.Name = {getMethod.Name}");
 
                 // Если get-метод текущего свойства равен null.
                 if (getMethod == null)
@@ -27,7 +46,7 @@ namespace WpfApplication42
                     ForegroundColor = ConsoleColor.DarkRed;
                     WriteLine($"{counter}) {getMethod.Name} равен null");
                 }
-                
+
                 // Если get-метод текущего свойства публичный.
                 if (getMethod.IsPublic)
                 {
@@ -43,6 +62,25 @@ namespace WpfApplication42
                 }
 
                 ForegroundColor = ConsoleColor.Gray;
+
+                // Добавление в словарь.
+
+                // Имя свойства класса LogMessage в нижнем регистре.
+                var propertyName = publicPropInfo.Name.ToLower();
+
+                // Поскольку у свойств нет параметров, поэтому в метод Invoke каждого свойства
+                // в качестве массива параметров передаётся null. 
+                // Строка getMethod.Invoke(message, null) по сути означает:
+                // вызвать свойство, хранимое в getMethod для объекта типа LogMessage. 
+
+                // Func<LogMessage, object> - это по сути делегат:
+                // public delegate TResult Func<in T, out TResult>(T arg).
+                Func<LogMessage, object> functor = (message) =>
+                {
+                    return getMethod.Invoke(message, null);
+                };
+
+                namedValues.Add(propertyName, functor);
             }
         }
     }
